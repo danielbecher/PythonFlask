4e4ab295c32cc41471aa12cf0c6fb66d0382e707
Daniel Becher <daniel@becher.com.br>
2022-01-17 09:40:14 -0300

    Aplicativo RastreioCorreios desenvolvimento em Python e framework Flask.

--- RastreioCorreios/webapp.py
+++ RastreioCorreios/webapp.py
@@ -3,10 +3,12 @@ from rastrearencomenda import rastrear
 
 app = Flask(__name__)
 
+#Cria a página inicial de consula
 @app.route('/', methods=['POST','GET'])
 def inicio():
     return render_template('index.html')
 
+#Cria a página de resposta da consulta
 @app.route('/consulta', methods=['GET','POST'])
 def consulta():
     codeguim = request.form.get('codigorastreio')
