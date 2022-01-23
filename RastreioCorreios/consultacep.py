4ed80f067e6400ab1fbfb641655cf9f899f20793
Daniel Becher <daniel@becher.com.br>
2022-01-23 14:40:20 -0300

    Adicionada a função de consulta de CEP e página inicial criada para dividir os dois assuntos e direcioná-los de acordo

---
+++ RastreioCorreios/consultacep.py
@@ -0,0 +1,15 @@
+#ConsultaCEP
+
+#importa as bibliotecas
+import requests
+
+def consultarCep(cep):
+    dadoscep = []
+    #verifica o cep válido
+    if len(cep) != 8:
+        return "CEP inválido!"
+    else:
+        request = requests.get('https://viacep.com.br/ws/{}/json'.format(cep))
+        dadosendereco = (request.json())
+        #Verifica se o CEP existe e retorna seus dados.
+        return dadosendereco
--- RastreioCorreios/webapp.py
+++ RastreioCorreios/webapp.py
@@ -1,18 +1,40 @@
+#Importa as bibliotecas e funções separadas necessárias
 from flask import Flask, render_template, request
 from rastrearencomenda import rastrear
+from consultacep import consultarCep
 
+
+#Cria o objeto app
 app = Flask(__name__)
 
-#Cria a página inicial de consula
+#Cria a página inicial "/"
 @app.route('/', methods=['POST','GET'])
 def inicio():
     return render_template('index.html')
 
-#Cria a página de resposta da consulta
-@app.route('/consulta', methods=['GET','POST'])
-def consulta():
+#Cria a página de rastreamento
+@app.route('/rastreio', methods=['POST','GET'])
+def inicioRastreio():
+    return render_template('rastreio.html')
+
+#Cria a página de resposta do rastreamento
+@app.route('/rastreio-consulta', methods=['GET','POST'])
+def rastreioConsulta():
     codeguim = request.form.get('codigorastreio')
     rastrear(codeguim)
-    return render_template('consulta.html', codeguim=codeguim, listaeventos2=rastrear(codeguim))
+    return render_template('rastreio-consulta.html', codeguim=codeguim, listaeventos2=rastrear(codeguim))
 
+#Cria a página de CEP
+@app.route('/cep', methods=['POST','GET'])
+def iniciocep():
+    return render_template('cep.html')
+
+#Cria a página de resposta do CEP
+@app.route('/cep-consulta', methods=['POST','GET'])
+def cepConsulta():
+    cep = request.form.get('cep')
+    consultarCep(cep)
+    return render_template('cep-consulta.html',cep=cep, dadoscep=consultarCep(cep))
+
+#Debug=True eu usei para que a cada salvamento do código, ele reiniciasse o app sem precisar dar reload.
 app.run(debug=True)
